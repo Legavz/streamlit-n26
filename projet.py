c1cf3882c4b1159ff6d0f52244e70fe77603128a
Legavz <113018046+Legavz@users.noreply.github.com>
2022-10-20 16:49:58 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -3,7 +3,9 @@ import random
 import math
 import streamlit as st
 
-df_releve = pd.read_csv("C:/Users/Alexis/Documents/Python/Projets/N26/releve_17_10.csv")
+uploaded_file = st.file_uploader("C:/Users/Alexis/Documents/Python/Projets/N26/releve_17_10.csv")
+
+df_releve = pd.read_csv(uploaded_file)
 
 df_releve_filtre = df_releve[df_releve["Montant (EUR)"] < 0]
 
