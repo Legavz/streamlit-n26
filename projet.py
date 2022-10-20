08dd5b1e4c5b53d447d4aa62adeec0cdd38e33d7
Legavz <113018046+Legavz@users.noreply.github.com>
2022-10-20 17:01:13 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -3,6 +3,16 @@ import random
 import math
 import streamlit as st
 
+
+"""
+Bienvenue sur les petits gestes !
+
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
+
 uploaded_file = st.file_uploader("Sélectionnez un fichier")
 
 df_releve = pd.read_csv(uploaded_file)
@@ -13,7 +23,7 @@ df_releve_filtre["Arrondis_depenses"] = df_releve_filtre["Montant (EUR)"].apply(
 
 df_releve_filtre['mois'] = pd.DatetimeIndex(df_releve_filtre['Date']).month
 
-mois = st.number_input('Quel mois arrondir ?')
+mois = st.number_input(label = 'Quel mois arrondir ?', min_value = 1, max_value = 12)
 
 montant = df_releve_filtre[df_releve_filtre['mois'] == mois]["Arrondis_depenses"].sum()
 
