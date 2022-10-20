1b8452cfbbd97aa4081b93e8dd7a869a4b4a2e6a
Legavz <113018046+Legavz@users.noreply.github.com>
2022-10-20 17:51:06 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -23,20 +23,21 @@ df_releve_filtre["Arrondis_depenses"] = df_releve_filtre["Montant (EUR)"].apply(
 
 df_releve_filtre['mois'] = pd.DatetimeIndex(df_releve_filtre['Date']).month
 
-periode = st.selectbox(
-    'Quelle période souhaitez-vous arrondir',
-    ('Un mois seulement', 'Plusieurs mois', 'La période entière'))
+    periode = st.selectbox(
+        'Quelle période souhaitez-vous arrondir',
+        ('Un mois seulement', 'Plusieurs mois', 'La période entière'))
 
-if periode == "Un mois seulement":
-  mois = st.number_input(label = 'Quel mois arrondir ?', min_value = df_releve_filtre['mois'].min(), max_value = df_releve_filtre['mois'].max())
-  montant = df_releve_filtre[df_releve_filtre['mois'] == mois]["Arrondis_depenses"].sum()
-  montant = round(montant, 1)
-  montant_defiscalise = montant * 0.33
-  st.write("Le montant de ton don s'élève à", montant, "euros")
-  st.write("En realité, cela ne te coutera que", montant_defiscalise)
-elif periode == "La période entière":
-  montant = df_releve_filtre["Arrondis_depenses"].sum()
-  montant = round(montant, 1)
-  montant_defiscalise = montant * 0.33
-  st.write("Le montant de ton don s'élève à", montant, "euros")
-  st.write("En realité, cela ne te coutera que", montant_defiscalise)
+    if periode == "Un mois seulement":
+      mois = st.number_input(label = 'Quel mois arrondir ?', min_value = df_releve_filtre['mois'].min(), max_value = df_releve_filtre['mois'].max())
+      montant = df_releve_filtre[df_releve_filtre['mois'] == mois]["Arrondis_depenses"].sum()
+      montant = round(montant, 1)
+      montant_defiscalise = round(montant * 0.33, 1)
+      st.write("Le montant de ton don s'élève à", montant, "€")
+      st.write("En realité, cela ne te coutera que", montant_defiscalise, "€")
+    elif periode == "La période entière":
+      montant = df_releve_filtre["Arrondis_depenses"].sum()
+      montant = round(montant, 1)
+      montant_defiscalise = round(montant * 0.33, 1)
+      st.write("Le montant de ton don s'élève à", montant, "€")
+      st.write("En realité, cela ne te coutera que", montant_defiscalise, "€")
+    elif periode == "Plusieurs mois" 
